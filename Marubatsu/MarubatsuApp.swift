7a8a4df7b43c2cfb481be06dfe1021aade070990
yonezawanatsuko <nabeta0915@gmail.com>
2025-11-08 08:35:19 +0900

    Initial Commit

---
+++ Marubatsu/MarubatsuApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarubatsuApp.swift
+//  Marubatsu
+//
+//  Created by yonezawanatsuko on 2025/11/08.
+//
+
+import SwiftUI
+
+@main
+struct MarubatsuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
